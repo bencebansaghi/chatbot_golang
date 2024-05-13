4568e035a070ebb7b77683f430a7db2c24046073
Bence Bánsághi <35151749+bencebansaghi@users.noreply.github.com>
2024-05-13 19:39:12 +0300

    better env, able to get posts from timeline

--- main.go
+++ main.go
@@ -2,32 +2,74 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/Davincible/goinsta/v3"
 	"github.com/joho/godotenv"
 )
 
-func loadDotenv() (string, string, error) {
-	err := godotenv.Load()
-	if err != nil {
-		return "", "", errors.New("error loading .env file")
+const (
+	configPath = "./.goinsta"
+)
+
+func loadEnvVar(varName string) (string, error) {
+	variable, ok := os.LookupEnv(varName)
+	if !ok {
+		return "", errors.New(varName + " not found in .env file")
 	}
-	username := os.Getenv("INSTAGRAM_USERNAME")
-	password := os.Getenv("INSTAGRAM_PASSWORD")
-	if username == "" || password == "" {
-		return "", "", errors.New("username or password not found in .env file")
+	if variable == "" {
+		return "", errors.New(varName + " is empty")
+	}
+	return variable, nil
+}
+
+func loadEnvInstaUserPass() (string, string, error) {
+	username, err := loadEnvVar("INSTAGRAM_USERNAME")
+	if err != nil {
+		return "", "", err
+	}
+	password, err := loadEnvVar("INSTAGRAM_PASSWORD")
+	if err != nil {
+		return "", "", err
 	}
 	return username, password, nil
 }
 
-func main() {
+func getPosts(insta *goinsta.Instagram) /*[]goinsta.Item*/ {
+	profilesStr, err := loadEnvVar("INSTA_PROFILES")
+	if err != nil {
+		log.Fatal(err)
+	}
+	profiles := strings.Split(profilesStr, ",")
+	getPost(insta, profiles[len(profiles)-1])
+}
 
-	insta, err := goinsta.Import("./.goinsta")
+func getPost(insta *goinsta.Instagram, profileStr string) /*goinsta.Item, */ {
+	profile, err := insta.VisitProfile(profileStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user := profile.User
+	fmt.Printf(
+		"%s has %d followers, %d posts, and %d IGTV vids\n",
+		profileStr, user.FollowerCount, user.MediaCount, user.IGTVCount,
+	)
+
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Panic("Error loading .env file")
+	}
+	insta, err := goinsta.Import(configPath)
 	if err != nil {
 		log.Println("Error loading .goinsta file, trying to login from dotenv")
-		username, password, err := loadDotenv()
+		username, password, err := loadEnvInstaUserPass()
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -38,6 +80,17 @@ func main() {
 	} else {
 		log.Println("Loaded .goinsta file")
 	}
+	err = insta.OpenApp()
+	if err != nil {
+		log.Fatal("Error opening app: ", err)
+	}
+	tl := insta.Timeline
+	for _, item := range tl.Items {
+		fmt.Println(item.Caption.Text)
+	}
 
-	defer insta.Export("./.goinsta")
+	// this dont work
+	getPosts(insta)
+
+	defer insta.Export(configPath)
 }
